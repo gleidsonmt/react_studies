b0c5fdcd6581d1ac5edcd5cc3c5aafc2aabc0aa4
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2024-04-29 15:34:35 -0300

    class 04

---
+++ curso_react/src/components/Frase.jsx
@@ -0,0 +1,7 @@
+export default function Frase () {
+    return (
+        <>
+            <p>Este eh um componente com uma frase</p>
+        </>
+    )
+}
--- curso_react/src/components/HelloWorld.jsx
+++ curso_react/src/components/HelloWorld.jsx
@@ -1,8 +1,10 @@
+import Frase from "./Frase";
 
 
 export default function HelloWorld() {
     return (
         <>
+            <Frase/>
             <h1>Meu Primeiro componente</h1>
         </>
     );
