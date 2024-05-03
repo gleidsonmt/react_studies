5aa95c7d1491ea26f2c74d3c4211b1208db7f3d8
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2024-05-03 13:24:23 -0300

    add skeleton ;

--- curso_react/src/components/Evento.jsx
+++ curso_react/src/components/Evento.jsx
@@ -1,12 +1,19 @@
+import Button from "./Button";
 
 export default function Evento({ numero }) {
-    function meuEvento() {
-        console.log(`Opa, fui ativado! ${numero}`)
-    }
-    return (
-        <div>
-        <p>Clique para disparar um evento"</p>
-        <button onClick={meuEvento}>Ativar!</button>
-        </div>
-    )
+  function meuEvento() {
+    console.log(`Opa, fui ativado! ${numero}`);
+  }
+
+  function segundoEvento() {
+    console.log("ativando o segundo evento");
+  }
+
+  return (
+    <div>
+      <p>Clique para disparar um evento"</p>
+      <Button text="Primeiro Evento" event={meuEvento} />
+      <Button text="Segundo Evento" event={segundoEvento} />
+    </div>
+  );
 }
